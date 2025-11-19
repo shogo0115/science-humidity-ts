79b12f2472c42e04ae465fa69db74ef4144705e0
shogo0115 <shogo34180115@gmail.com>
2025-11-20 05:17:34 +0900

    webpackの修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-// webpack.config.js
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: { bundle: './src/index.tsx' },
@@ -12,14 +12,21 @@ module.exports = {
   devtool: 'source-map',
   resolve: { extensions: ['.tsx', '.ts', '.js'] },
   devServer: {
-  static: { directory: path.join(__dirname, 'public'), watch: true },
-  historyApiFallback: true,
-  port: 8080,
-},
+    static: { directory: path.join(__dirname, 'public'), watch: true },
+    historyApiFallback: true,
+    port: 8080,
+  },
   module: {
     rules: [
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
-      { test: /\.css$/,  use: ['style-loader', 'css-loader'] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
